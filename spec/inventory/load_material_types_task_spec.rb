bbdef0bc320db61dbf58d467fcf1530e7bd5a30d
Jeremy Nelson <jermnelson@gmail.com>
2022-05-16 11:08:28 -0600

    Tests loading material types

---
+++ spec/inventory/load_material_types_task_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rake'
+require 'spec_helper'
+
+describe 'material types rake tasks' do
+  let(:load_material_types_task) { Rake.application.invoke_task 'inventory:load_material_types' }
+
+  before do
+    stub_request(:post, 'http://example.com/authn/login')
+      .with(body: Settings.okapi.login_params.to_h)
+
+    stub_request(:post, 'http://example.com/material-types')
+  end
+
+  context 'when loading material types' do
+    let(:material_types_csv) { load_material_types_task.send(:material_types_csv) }
+
+    it 'creates the hash key and value for a material type name' do
+      expect(load_material_types_task.send(:material_types_csv)[3]['name']).to eq 'accessories 4'
+    end
+  end
+end
