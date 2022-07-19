85c551dfb4858ecf05058739cb083a03633bd8ad
Joshua Greben <jgreben@stanford.edu>
2022-07-19 09:04:00 -0700

    Changes made after 7-15 dev folio migration load

--- tasks/helpers/folio_request.rb
+++ tasks/helpers/folio_request.rb
@@ -9,6 +9,10 @@ module FolioRequestHelper
   def trim_hash(hash, name)
     hash[name].each do |obj|
       obj.delete('metadata')
+      obj.delete('childOf')
+      obj.delete('grantedTo')
+      obj.delete('dummy')
+      obj.delete('deprecated')
     end
     hash.delete('totalRecords')
     hash.delete('resultInfo')
