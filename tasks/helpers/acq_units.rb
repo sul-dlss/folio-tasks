35e760ff001f4e657b37e055cbe31de56d8d0cd9
Joshua Greben <jgreben@stanford.edu>
2022-07-26 08:53:11 -0700

    Refactor acq units to avoid repeated top level variable warning

--- tasks/helpers/acq_units.rb
+++ tasks/helpers/acq_units.rb
@@ -28,4 +28,35 @@ module AcquisitionsUnitsTaskHelpers
   def acq_units_post(obj)
     @@folio_request.post('/acquisitions-units-storage/units', obj.to_json)
   end
+
+  def acq_unit_hash
+    acq_unit_hash = {}
+    @@folio_request.get('/acquisitions-units/units')['acquisitionsUnits'].each do |obj|
+      acq_unit_hash[obj['name']] = obj['id']
+    end
+    acq_unit_hash
+  end
+
+  def membership_hash
+    membership_hash = {}
+    @@folio_request.get('/acquisitions-units/memberships?limit=10000')['acquisitionsUnitMemberships'].each do |obj|
+      key = obj['acquisitionsUnitId'] + obj['userId']
+      membership_hash[key] = obj['id']
+    end
+    membership_hash
+  end
+
+  def acq_units_assign
+    user_acq_units_and_permission_sets_tsv.each do |obj|
+      acq_unit = obj['Acq Unit']
+      acq_unit_id = acq_unit_hash[acq_unit]
+      users = user_get(obj['SUNetID'])
+      users && users['users'].each do |user|
+        if acq_unit_id && !membership_hash[acq_unit_id + user['id']]
+          acq_membership_obj = { 'userId' => user['id'], 'acquisitionsUnitId' => acq_unit_id }
+          @@folio_request.post('/acquisitions-units/memberships', acq_membership_obj.to_json)
+        end
+      end
+    end
+  end
 end
--- tasks/users/assign_acquisition_units.rake
+++ tasks/users/assign_acquisition_units.rake
@@ -3,29 +3,10 @@
 require_relative '../helpers/tsv_user'
 
 namespace :tsv_users do
-  include TsvUserTaskHelpers
+  include TsvUserTaskHelpers, AcquisitionsUnitsTaskHelpers
 
   desc 'assign acquisition units to a user'
   task :assign_acquisition_units do
-    acq_unit_hash = {}
-    @@folio_request.get('/acquisitions-units/units')['acquisitionsUnits'].each do |obj|
-      acq_unit_hash[obj['name']] = obj['id']
-    end
-    membership_hash = {}
-    @@folio_request.get('/acquisitions-units/memberships?limit=10000')['acquisitionsUnitMemberships'].each do |obj|
-      key = obj['acquisitionsUnitId'] + obj['userId']
-      membership_hash[key] = obj['id']
-    end
-    user_acq_units_and_permission_sets_tsv.each do |obj|
-      acq_unit = obj['Acq Unit']
-      acq_unit_id = acq_unit_hash[acq_unit]
-      users = user_get(obj['SUNetID'])
-      users && users['users'].each do |user|
-        if acq_unit_id && !membership_hash[acq_unit_id + user['id']]
-          acq_membership_obj = { 'userId' => user['id'], 'acquisitionsUnitId' => acq_unit_id }
-          @@folio_request.post('/acquisitions-units/memberships', acq_membership_obj.to_json)
-        end
-      end
-    end
+    acq_units_assign
   end
 end
