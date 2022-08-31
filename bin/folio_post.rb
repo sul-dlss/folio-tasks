903379050fbcafcad5cb0a5a5d219095ea68a38c
Joshua Greben <jgreben@stanford.edu>
2022-08-31 11:40:46 -0700

    Allow a post without a body

--- bin/folio_post.rb
+++ bin/folio_post.rb
@@ -4,6 +4,6 @@ require_relative '../lib/folio_request'
 require 'json'
 
 path = ARGV[0]
-json = JSON.parse(File.read(ARGV[1]))
+ARGV[1] && json = JSON.parse(File.read(ARGV[1]))
 folio = FolioRequest.new
-folio.post(path, json.to_json)
+json ? folio.post(path, json.to_json) : folio.post_no_body(path)
