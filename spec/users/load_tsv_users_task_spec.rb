581d7b462404854e8340c3e1a282b1660f563dcf
Joshua Greben <jgreben@stanford.edu>
2022-10-07 12:02:45 -0700

    lint fix

--- spec/users/load_tsv_users_task_spec.rb
+++ spec/users/load_tsv_users_task_spec.rb
@@ -11,7 +11,7 @@ describe 'loading tsv users who do not have registry ids' do
 
   before do
     stub_request(:post, 'http://example.com/authn/login')
-    .with(body: Settings.okapi.login_params.to_h)
+      .with(body: Settings.okapi.login_params.to_h)
 
     stub_request(:post, 'http://example.com/user-import')
     stub_request(:post, 'http://example.com/users')
