bd37006605482b38d56a4bba713987eccdd75222
Shelley Doljack <sdoljack@stanford.edu>
2023-08-02 16:32:24 -0600

    Adds a test for loading calendars.

--- spec/tenant/load_tenant_settings_task_spec.rb
+++ spec/tenant/load_tenant_settings_task_spec.rb
@@ -46,7 +46,7 @@ describe 'tenant settings rake tasks' do
       .with(query: hash_including)
       .to_return(body: '{ "locations": [{ "id": "abc-123", "name": "Green Stacks", "code": "GREEN" }] }')
 
-    stub_request(:post, 'http://example.com/calendars/calendar')
+    stub_request(:post, 'http://example.com/calendar/calendars')
   end
 
   context 'when loading institutions' do
@@ -245,11 +245,16 @@ describe 'tenant settings rake tasks' do
   end
 
   context 'when creating calendars' do
-    let(:calendars_json) { load_course_status.send(:calendars_json) }
+    let(:calendars_json) { load_calendars.send(:calendars_json) }
 
     # new json schemas not available for mod-calendar
     xit 'supplies valid json for posting calendars' do
       expect(calendars_json['calendars'].sample).to match_json_schema('mod-calendar', 'calendars')
+    end
+
+    it 'posts calendar json data' do
+      expect(load_calendars).to have_requested(:post, 'http://example.com/calendar/calendars')
+        .with(body: '{"id":"22b40887-20a4-4927-a2da-5886a6c6ea43","name":"Law Book Cabinet - Course Reserves","startDate":"1992-01-01","endDate":"2040-12-31","assignments":["0939d005-3e55-46d4-ab36-6f9099675876"],"normalHours":[{"startDay":"SUNDAY","startTime":"00:00:00","endDay":"SATURDAY","endTime":"23:59:00"}],"exceptions":[]}')
     end
   end
 end
