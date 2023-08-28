8d26edd7a1df117da85d9d5248bab9c7e6d2894f
Joshua Greben <jgreben@stanford.edu>
2023-08-28 12:07:56 -0700

    Remove duplicate value in shared dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :log_level, :info
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/s/SUL/Bin/#{fetch(:application)}"
 
-set :linked_dirs, %w[config/settings jar etc certs log out jar lib WebLogic_lib]
+set :linked_dirs, %w[config/settings jar etc certs log out lib WebLogic_lib]
 
 # Default value for keep_releases is 5
 set :keep_releases, 3
