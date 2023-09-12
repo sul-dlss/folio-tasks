ad2c70911f37653dce647b5d12605f09f9ac1458
Joshua Greben <jgreben@stanford.edu>
2023-09-12 14:07:44 -0700

    Allow date arg for illiad userload script

--- run/illiad-userload.sh
+++ run/illiad-userload.sh
@@ -5,6 +5,6 @@ source $(dirname $0)/harvest.env
 [[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 cd $HARVEST_HOME
-STAGE=$STAGE bundle exec rake illiad:fetch_and_load_users > $LOG/illiad-userload.log 2>&1
+STAGE=$STAGE bundle exec rake illiad:fetch_and_load_users[$1] > $LOG/illiad-userload.log 2>&1
 
 cat $LOG/illiad-userload.log | mailx -s 'ILLiad Userload Errors' sul-unicorn-devs@lists.stanford.edu
