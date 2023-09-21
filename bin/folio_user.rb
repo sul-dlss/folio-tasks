ab1a0f2093ec8a17d61944350b60873275f658c8
Joshua Greben <jgreben@stanford.edu>
2023-09-20 18:45:20 -0700

    Use correct Person path for folio_user script

--- bin/folio_user.rb
+++ bin/folio_user.rb
@@ -17,7 +17,7 @@ folio_response = folio.post('/user-import', user_json)
 puts user_json
 
 # File used for emailing the user import result
-File.open('log/user-import-response.log', 'a') do |f|
+File.open('Person/log/user-import-response.log', 'a') do |f|
   log_json = {
     'batch_number' => ARGV[1],
     'batch_response' => folio_response,
