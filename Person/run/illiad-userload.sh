44c98a1a9ab0ed1dd798c2e44b227a326501ccc9
Joshua Greben <jgreben@stanford.edu>
2023-09-25 09:56:15 -0700

    Remove newlines from illiad log

--- Person/run/illiad-userload.sh
+++ Person/run/illiad-userload.sh
@@ -14,5 +14,8 @@ cd $HARVEST_HOME
 
 java -jar target/Person-jar-with-dependencies.jar $folio_users >> $LOG/illiad-userload.log  2>> $LOG/illiad-err.log
 
+# Remove carriage returns from the log
+sed -i 's/\r//g' $LOG/illiad-userload.log
+
 cat $LOG/illiad-userload.log | sed '/\n/d' | mailx -s 'ILLiad Userload Logs' sul-unicorn-devs@lists.stanford.edu
 cat $LOG/illiad-err.log | mailx -s 'ILLiad Userload Errors' sul-unicorn-devs@lists.stanford.edu
