62361f732de92d7e90c63fe09702dbead9871e43
Joshua Greben <jgreben@stanford.edu>
2023-09-25 10:52:59 -0700

    Use a date arg for illiad userload and rm carriage returns

--- Person/run/illiad-userload.sh
+++ Person/run/illiad-userload.sh
@@ -4,18 +4,20 @@ source $(dirname $0)/harvest.env
 
 [[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-if [ -z $1 ]; then
-  folio_users=$LOG/folio-user.log
-else
-  folio_users=$LOG/folio-user.log.$1
+if [[ -n $1 ]]; then
+  DATE=$(basename $1 | cut -d. -f 3)
+  LOG_DATE=".$DATE"
 fi
+
+folio_users=$LOG/folio-user.log$LOG_DATE
 
 cd $HARVEST_HOME
 
 java -jar target/Person-jar-with-dependencies.jar $folio_users >> $LOG/illiad-userload.log  2>> $LOG/illiad-err.log
 
 # Remove carriage returns from the log
-sed -i 's/\r//g' $LOG/illiad-userload.log
+sed -i 's/\r//g' $LOG/illiad-userload.log$LOG_DATE
 
-cat $LOG/illiad-userload.log | sed '/\n/d' | mailx -s 'ILLiad Userload Logs' sul-unicorn-devs@lists.stanford.edu
-cat $LOG/illiad-err.log | mailx -s 'ILLiad Userload Errors' sul-unicorn-devs@lists.stanford.edu
+cat $LOG/illiad-userload.log$LOG_DATE | mailx -s 'ILLiad Userload Logs' sul-unicorn-devs@lists.stanford.edu
+
+cat $LOG/illiad-err.log$LOG_DATE | mailx -s 'ILLiad Userload Errors' sul-unicorn-devs@lists.stanford.edu
