700460ee21095cfaa1ce30504e1e881d8bc03eb7
Shelley Doljack <sdoljack@stanford.edu>
2026-07-01 18:04:45 -0600

    Updates FolioRequest to handle full responses from updated folio_client gem.

--- lib/folio_request.rb
+++ lib/folio_request.rb
@@ -5,7 +5,7 @@ require 'config'
 require 'http'
 Config.load_and_set_settings(Config.setting_files('config', ENV['STAGE'] || 'dev'))
 
-# Class to use HTTP actions with FOLIO
+# Class to use FolioClient actions with FOLIO
 class FolioRequest
   def client
     FolioClient.configure(
@@ -17,77 +17,72 @@ class FolioRequest
   end
 
   def get(path)
-    response = client.get(path)
-    pp response unless response.nil?
+    handle_folio_request do
+      pp client.get(path)
+    end
   end
 
   def get_json(path)
-    puts JSON.pretty_generate(client.get(path))
+    handle_folio_request do
+      puts JSON.pretty_generate(client.get(path))
+    end
   end
 
   def get_cql(path, query)
     path += "?query=#{CGI.escape(query)}"
-    response = client.get(path)
-    pp response unless response.nil?
+    handle_folio_request do
+      pp client.get(path)
+    end
   end
 
   def get_cql_json(path, limit, query)
     path += "?limit=#{limit}&query=#{CGI.escape(query)}"
-    puts JSON.pretty_generate(client.get(path))
+    handle_folio_request do
+      puts JSON.pretty_generate(client.get(path))
+    end
   end
 
-  def post(path, json = nil)
-    response = client.post(path, json)
-    pp response unless response.nil?
+  def post(path, json = nil, **other)
+    handle_folio_request do
+      client.post(path, json) do |resp|
+        parse(resp, **other)
+      end
+    end
   end
 
-  def put(path, json = nil)
-    response = client.put(path, json)
-    pp response unless response.nil?
+  def put(path, json = nil, **other)
+    handle_folio_request do
+      client.put(path, json) do |resp|
+        parse(resp, **other)
+      end
+    end
   end
 
   def delete(path, **other)
-    parse(authenticated_request(path, method: :delete), **other)
+    handle_folio_request do
+      client.delete(path) do |resp|
+        parse(resp, **other)
+      end
+    end
+  end
+
+  def handle_folio_request
+    yield
+  rescue FolioClient::Error => e
+    puts "FolioClient Error: #{e.message}"
+    nil
   end
 
   def parse(response, **other)
     if other[:response_code]
-      response.code
+      puts response.status
     elsif other[:no_response]
       ''
     else
-      pp JSON.parse(response)
+      pp JSON.parse(response.body) unless response.body.empty?
     end
   rescue JSON::ParserError
     puts response
-  end
-
-  def session_token
-    @session_token ||= begin
-      response = request('/authn/login', json: Settings.okapi.login_params, method: :post)
-      response.parse(:json)['okapiToken']
-    end
-  end
-
-  def authenticated_request(path, headers: {}, **other)
-    request(path, headers: headers.merge('x-okapi-token': session_token), **other)
-  end
-
-  def request(path, headers: {}, method: :get, **other)
-    default_headers = {
-      accept: 'application/json, text/plain',
-      content_type: 'application/json',
-      user_agent: Settings.okapi.user_agent,
-      'x-okapi-tenant': Settings.okapi.tenant_id
-    }
-    HTTP
-      .timeout(150)
-      .headers(default_headers.merge(headers))
-      .request(method, base_url + path, **other)
-  end
-
-  def base_url
-    Settings.okapi.url
   end
 
   def make_path(path)
